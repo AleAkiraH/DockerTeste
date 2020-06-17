3d2e326dfbbdf7da149c05373f5da431505d2c60
AleAkiraH <alexsandersf15@gmail.com>
2020-06-17 19:42:29 -0300

    aaa

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,20 @@
-from flask import Flask
+# Libs necessárias para utilização dessa api
+# Libs required to use this api
+# Libs requeridas para usar esta API
+# Libs Externas
+# Libs External
+# Libs externa
+from flask import Flask, jsonify, request
+import json
+import datetime
+import base64
+import uuid
+import os
+import math
+# Libs Internas
+# Libs Internal
+# Libs interna
+from app import api_classes
+from app import api_funcoes
 app = Flask(__name__)
-from app import views
+from app import FaceMatchMain
