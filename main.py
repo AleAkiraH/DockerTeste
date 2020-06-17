85998ce6cdf496297776bc509e0faff0774c7656
AleAkiraH <alexsandersf15@gmail.com>
2020-06-17 20:33:41 -0300

    cccc

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from app import app
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return "hello world!"
+
+@app.route('/a')
+def a():
+   return "Func A Funcionou"
+
+@app.route('/a/b')
+def ab():
+   return "Func A/B Funcionou"
