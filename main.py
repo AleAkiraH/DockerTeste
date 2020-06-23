878749676108c097c5994dfab09ac3aa2ba4ee07
AleAkiraH <alexsandersf15@gmail.com>
2020-06-23 20:23:19 -0300

    sadsa

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def getsimples1():
 	return "hello world 1"
     
-# @app.route("/2", methods=['GET'])
-# def getsimples2():
-# 	return "<!DOCTYPE html><html><body><h2>Wagnao Voando</h2><img src="https://i.pinimg.com/236x/a6/63/57/a6635733f965b25b69e2d263b541e022.jpg" alt="Trulli" width="500" height="333"></body></html>"
+@app.route("/2", methods=['GET'])
+def getsimples2():
+	return "<!DOCTYPE html><html><body><h2>Wagnao Voando</h2></body></html>"
     
 @app.route("/", methods=['GET'])
 def getsimples():
